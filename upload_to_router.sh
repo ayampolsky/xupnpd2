7a9cb3e3446be562c73dc4f961a892b189da7b27
Alexander Yampolsky <yampa@ya.ru>
2021-09-13 22:17:49 +0500

    Added selection of bzip2 block size with best compression.

--- upload_to_router.sh
+++ upload_to_router.sh
@@ -8,12 +8,25 @@ if [[ "$ROUTER_IP_ADDRESS" == "" ]]; then
 	exit 0
 fi
 
-tar -cjvf etc/xupnpd2.tar.bz2 \
-	xupnpd \
-	www/ \
-	media/ \
-	xupnpd.cfg \
-	get_playlist_planeta.sh
+FILE_LIST="xupnpd www/ media/ xupnpd.cfg get_playlist_planeta.sh"
+
+function get_bzip2_block_size_with_best_compression_ratio {
+	COMPRESSION_DIR="/tmp/xupnpd2_compression/"
+	rm -rf "$COMPRESSION_DIR"
+	mkdir "$COMPRESSION_DIR"
+	for i in `seq 9`; do
+		export "BZIP=-$i"
+		tar -cjvf "$COMPRESSION_DIR/xupnpd2_$i.tar.bz2" $FILE_LIST > /dev/null
+	done
+	BZIP_ARG=`ls -S $COMPRESSION_DIR/xupnpd2_*.tar.bz2 | tail -n 1 | grep -o "xupnpd2_[0-9]" | cut -d '_' -f 2`
+	rm -rf "$COMPRESSION_DIR"
+	echo "$BZIP_ARG"
+}
+
+BZIP_ARG="-`get_bzip2_block_size_with_best_compression_ratio`"
+echo "bzip2 argument is \"$BZIP_ARG\""
+export "BZIP=$BZIP_ARG";
+tar -cjvf etc/xupnpd2.tar.bz2 $FILE_LIST
 
 #cp xupnpd.cfg etc/
 
